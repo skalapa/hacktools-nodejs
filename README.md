ea13f65daa7e937fa86574dbb1e8a649773a3ded
bsherwin <brian@surewinsolutions.com>
2015-03-03 14:43:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jade-templating
+Copied from node-crash-course so I know it will exist for a walkthrough.
---
+++ routes/recipes.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var router = express.Router();
+var recipes = require('../data/recipeData');
+/* Renders Recipe view */
+router.get('/:id', function (req, res) {
+    var kind = req.params.id;
+    
+    res.render('recipes', {
+        recipes: {
+            list: recipes[kind],
+            kind: recipes.recipeTypeName[kind]
+        }
+    });
+});
+
+module.exports = router;
